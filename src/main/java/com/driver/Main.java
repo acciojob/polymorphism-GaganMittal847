8a880116d4008bf92c203321f98e6b524acc31d8
GaganMittal847 <gaganmittal048@gmail.com>
2023-02-09 10:56:33 +0530

    polymorphism ass2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
+    static class Product{
+
+        public int Product(int x,int y){
+           return x*y;
+        }
+        public int Product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double Product(double x,double y){
+            return x*y;
+        }
+    }
+    public static void main(String[] args){
+        Product p = new Product();
+        p.Product(1,2);
+        p.Product(1,2,3);
+        p.Product(3.0,4.0);
+    }
 
 }
