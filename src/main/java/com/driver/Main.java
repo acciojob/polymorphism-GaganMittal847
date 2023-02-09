b4d5cc10e2c59f7548d124da6df0879936c8db21
GaganMittal847 <gaganmittal048@gmail.com>
2023-02-09 11:11:04 +0530

    overloading ass2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,23 +1,23 @@
 package com.driver;
 
 public class Main {
-    static class Product{
+    public static class Product{
 
-        public int Product(int x,int y){
+        public int product(int x,int y){
            return x*y;
         }
-        public int Product(int x,int y,int z){
+        public int product(int x,int y,int z){
             return x*y*z;
         }
-        public double Product(double x,double y){
+        public double product(double x,double y){
             return x*y;
         }
     }
     public static void main(String[] args){
         Product p = new Product();
-        p.Product(1,2);
-        p.Product(1,2,3);
-        p.Product(3.0,4.0);
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(3.0,4.0);
     }
 
 }
